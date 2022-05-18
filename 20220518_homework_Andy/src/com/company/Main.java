6b064815e0ee0c7b2ccd9c2777843c00bdc97ca4
Olga O <zorina.olya@gmail.com>
2022-05-19 00:26:44 +0200

    Java homework for 19.05

---
+++ 20220518_homework_Andy/src/com/company/Main.java
@@ -0,0 +1,79 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3, 2},
+                {2, -4, -6, 30, 57},
+                {9, 8, -300}
+        };
+
+        System.out.println(checkIfAverageMoreZero(arr, 1));
+
+        printArray(flatArray(arr));
+    }
+
+    /**
+     * Task 2 Implement the method that takes int[][] array and line number and check checks that the
+     * average of the elements in a row is greater than 0
+     */
+    public static boolean checkIfAverageMoreZero(int[][] array, int num) {
+        int sum = 0;
+
+        // проверить что num меньше, чем количество строк массива
+        if (num > array.length - 1) {
+            return false;
+        }
+
+        // сделать цикл по строке массива num, array[num] от 0 до array[num].length
+        for (int i = 0; i < array[num].length; i++) {
+            // посчитать сумму элементов и поделить на количество
+            sum += array[num][i];
+        }
+
+        // если больше 0 вернуть true / иначе false
+        return sum / array[num].length > 0;
+    }
+
+    /**
+     * Task 3 Implement the method that takes a two-dimensional array and return the single-dimensional
+     * array with all elements of the given array
+     * ({ {1,2},{3,4},{5,6} }) -> {1,2,3,4,5,6}
+     * <p>
+     * Реализуйте метод, который принимает двумерный массив и возвращает одномерный массив со всеми
+     * элементами данного массива. ({{1,2},{3,4},{5,6}}) -> {1,2,3,4,5,6}
+     */
+    public static int[] flatArray(int[][] inputArray) {
+        // считаем общее количество элементнов в 2х мерном массиве
+        int totalElements = 0;
+
+        for (int i = 0; i < inputArray.length; i++) { //строка
+            totalElements += inputArray[i].length;
+        }
+
+        // создать одномерных массив с длинной = общему количеству элементов
+        int[] result = new int[totalElements];
+
+        // проходим по каждому элементу 2х мерного массива и добавляем его в нужный индекс
+        // одномерного массива
+        int index = 0;
+
+        for (int i = 0; i < inputArray.length; i++) {
+            for (int j = 0; j < inputArray[i].length; j++) {
+                result[index] = inputArray[i][j];
+                index++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+}
